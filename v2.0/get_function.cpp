3883f46c15a934a74bf9311d025112373b4a9ad1
SARTHAK811 <sarthakk20@iitk.ac.in>
2023-02-28 19:51:49 +0530

    get_function_things_added

--- v2.0/get_function.cpp
+++ v2.0/get_function.cpp
@@ -13,6 +13,65 @@ const ld PI = acos(-1);
 const ld EPS = 1e-9;
 const ll INF = 1e18;
 
+void adding_separators(){
+    seperator.insert("COMMA");
+    seperator.insert("DOT");
+    seperator.insert("TRIPLE_DOT");
+    seperator.insert("LEFT_PARANTHESIS");
+    seperator.insert("RIGHT_PARENTHESIS");
+    seperator.insert("LEFT_CURLY_BRACE");
+    seperator.insert("RIGHT_CURLY_BRACE");
+    seperator.insert("LEFT_SQUARE_BRACE");
+    seperator.insert("RIGHT_SQUARE_BRACE");
+    seperator.insert("SEMI_COLON");
+    seperator.insert("DOUBLE_COLON");
+}
+
+void adding_literals(){
+    literal.insert("CHARACTER_LITERAL");
+    literal.insert("BOOLEAN_LITERAL");
+    literal.insert("FLOATINGPOINT_LITERAL");
+    literal.insert("INTEGER_LITERAL");
+    literal.insert("NULL_LITERAL");
+    literal.insert("STRING_LITERAL");
+    literal.insert("TEXTBLOCK");
+}
+
+void adding_keywords(){
+    keyword.insert("ASSERT");
+    keyword.insert("BOOLEAN_TYPE");
+    keyword.insert("BREAK");
+    keyword.insert("CATCH");
+    keyword.insert("CLASS");
+    keyword.insert("CONTINUE");
+    keyword.insert("ELSE");
+    keyword.insert("EXTENDS");
+    keyword.insert("FINAL");
+    keyword.insert("FINALLY");
+    keyword.insert("FLOAT_POINT_TYPE");
+    keyword.insert("FOR");
+    keyword.insert("IF");
+    keyword.insert("IMPLEMENTS");
+    keyword.insert("IMPORT");
+    keyword.insert("INTEGRAL_TYPE");
+    keyword.insert("INTERFACE");
+    keyword.insert("NEW");
+    keyword.insert("PERMITS");
+    keyword.insert("PRIVATE");
+    keyword.insert("PUBLIC");
+    keyword.insert("RETURN");
+    keyword.insert("STATIC");
+    keyword.insert("SUPER");
+    keyword.insert("SYNCHRONIZED");
+    keyword.insert("THIS");
+    keyword.insert("THROW");
+    keyword.insert("THROWS");
+    keyword.insert("TRY");
+    keyword.insert("VAR");
+    keyword.insert("VOID");
+    keyword.insert("WHILE");
+    keyword.insert("YIELD");
+}
 void initiate(map<string,string> &m){
     m.insert(make_pair("DotIdentifiers","Name"));
     m.insert(make_pair("ClassModifiers","Modifiers"));
@@ -26,6 +85,9 @@ int main(){
     fstream fin,fout;
     fin.open("input.txt", ios::in);
     fout.open("lexer_with_func.txt",ios::out);
+    adding_keywords();
+    adding_literals();
+    adding_separators();
     initiate(m);
     string s,temp;
     int flag=0;
