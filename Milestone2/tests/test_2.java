fba1fea84b689c28de43630811b4e5f8f45d0479
Samadeol <samartha20@iitk.ac.in>
2023-03-15 04:30:39 +0530

    starting work

---
+++ Milestone2/tests/test_2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        int x=5;
+        int k = abc ? x>0 : 1;
+        double num = (double)k;
+        ++x;
+    }
+}
